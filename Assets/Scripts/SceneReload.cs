df4a517a0aed66ab6762becdf6a350c11ce4510a
ryuya7 <nagamon1946730@gmail.com>
2020-04-02 00:32:14 +0900

    棒と殴打のテスト完了

---
+++ Assets/Scripts/SceneReload.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReload : MonoBehaviour
+{
+    void Update(){
+        if(OVRInput.GetDown(OVRInput.RawButton.Y) | OVRInput.GetDown(OVRInput.RawButton.B)){
+            SceneManager.LoadScene("TestScene");
+        }
+    }
+}
