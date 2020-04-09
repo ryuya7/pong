e10d391b86c7df73b0f18a88c602a8bfd651dc20
ryuya7 <nagamon1946730@gmail.com>
2020-04-10 01:39:59 +0900

    球が後ろの壁に衝突したら停止する処理を実装

---
+++ Assets/Scripts/BallThroughed.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThroughed : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other){
+        other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
