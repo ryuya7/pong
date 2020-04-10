f3f234866481ab039e3b913d8ba551f12cc064dd
ryuya7 <nagamon1946730@gmail.com>
2020-04-11 02:21:40 +0900

    スコア機能の実装

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text ScoreText;
+    public Text HighScoreText;
+    public GameObject Sphere;
+    private Rigidbody rigid;
+    private float Speed;
+    private float TimeDeltaTime;
+    private float Product;
+    public int Coefficient; //100以上の値を設定する。
+    private int Score;
+    private int HighScore;
+    private string HighScoreKey = "HighScore";
+
+    void Start(){
+        Initialize();
+        rigid = Sphere.GetComponent<Rigidbody>();
+    }
+
+    void Update(){
+        if(HighScore < Score){
+            HighScore = Score;
+        }
+        TimeDeltaTime = Time.deltaTime;
+        Speed = rigid.velocity.magnitude;
+        Product = Speed * Speed * TimeDeltaTime * Coefficient;
+        Score = Score + (int)Product;
+        ScoreText.text = Score.ToString();
+        HighScoreText.text = HighScore.ToString();
+    }
+
+    private void Initialize(){
+        Score = 0;
+        HighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+    }
+
+    public void Save(){
+        PlayerPrefs.SetInt(HighScoreKey, HighScore);
+        PlayerPrefs.Save();
+        Initialize();
+    }
+}
